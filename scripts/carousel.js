dbb65abe3d432e995bdae33e6e83b99529370ce3
Senchi3 <sensensenchi@gmail.com>
2021-09-10 13:17:28 -0500

    A shitfuckton of changes to try and get the carousel to work

---
+++ scripts/carousel.js
@@ -0,0 +1,13 @@
+import bulmaCarousel from '../extensions/bulma-carousel.min.js';
+
+// Initialize all elements with carousel class.
+const carousels = bulmaCarousel.attach('.carousel', options);
+
+// To access to bulmaCarousel instance of an element
+const element = document.querySelector('#my-element');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+	element.bulmaCarousel.on('show', function(bulmaCarouselInstance) {
+		console.log(bulmaCarouselInstance.index);
+	});
+}
