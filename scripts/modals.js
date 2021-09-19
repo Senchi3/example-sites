e70c3a3aebc9576027ad93cbd70d8a6330de0121
Senchi3 <sensensenchi@gmail.com>
2021-09-18 19:32:11 -0500

    Modal update

--- scripts/modals.js
+++ scripts/modals.js
@@ -1,11 +1,15 @@
 var modals = document.querySelectorAll(".modal");
-var openButtons = document.querySelectorAll("#modal-open");
+var openButtons = document.querySelectorAll(".modal-open");
 var closeButtons = document.querySelectorAll(".modal-close");
+
+console.log(modals);
+console.log(openButtons);
+console.log(closeButtons);
 
 for (let i = 0; i < modals.length; i++) {
   console.log(modals[i]);
   console.log(openButtons[i]);
-  console.log(openButtons[i]);
+  console.log(closeButtons[i]);
 
   openButtons[i].addEventListener("click", function () {
     modals[i].classList.add("is-active");
