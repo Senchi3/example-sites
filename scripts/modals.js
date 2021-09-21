e7115a1cbb9ad3b41fe8048654692e2dd39594fd
Senchi3 <sensensenchi@gmail.com>
2021-09-21 08:30:16 -0500

    Modals work now, classes get removed when carousel is used

--- scripts/modals.js
+++ scripts/modals.js
@@ -1,21 +1,18 @@
 var modals = document.getElementsByClassName("modal");
+var modalBackgrounds = document.getElementsByClassName("modal-background");
 var openButtons = document.getElementsByClassName("modal-open");
 var closeButtons = document.getElementsByClassName("modal-close");
 
-console.log(modals);
-console.log(openButtons);
-console.log(closeButtons);
-
 for (let i = 0; i < modals.length; i++) {
-  console.log(modals[i]);
-  console.log(openButtons[i]);
-  console.log(closeButtons[i]);
-
   openButtons[i].addEventListener("click", function () {
     modals[i].classList.add("is-active");
   });
 
   closeButtons[i].addEventListener("click", function () {
+    modals[i].classList.remove("is-active");
+  });
+
+  modalBackgrounds[i].addEventListener("click", function () {
     modals[i].classList.remove("is-active");
   });
 }
